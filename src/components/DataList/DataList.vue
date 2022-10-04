c51931a9f05f12a4be69f995c6efbf31d8f92d30
Zahirw <wakidzahir@gmail.com>
2022-10-04 13:07:46 +0700

    change and add wording at home and activity detail

--- src/components/DataList/DataList.vue
+++ src/components/DataList/DataList.vue
@@ -38,27 +38,27 @@
         <div class="border-t border-gray-200">
           <dl>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Full name</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+              <dt class="text-sm font-medium text-gray-500">Nama Proyek</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Sipkons</dd>
             </div>
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Application for</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Backend Developer</dd>
+              <dt class="text-sm font-medium text-gray-500">Lokasi</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Kelurahan Kebon Bawang</dd>
             </div>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Email address</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+              <dt class="text-sm font-medium text-gray-500">RW</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">RW 04, RW 06</dd>
             </div>
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Salary expectation</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">$120,000</dd>
+              <dt class="text-sm font-medium text-gray-500">Kontraktor Pelaksana</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">PT. JAYA</dd>
             </div>
             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">About</dt>
-              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.</dd>
+              <dt class="text-sm font-medium text-gray-500">Konsultan Pengawas</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Billy</dd>
             </div>
             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Attachments</dt>
+              <dt class="text-sm font-medium text-gray-500">Progres Kegiatan Saat Ini</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                 <ul role="list" class="divide-y divide-gray-200 rounded-md border border-gray-200">
                   <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
@@ -67,7 +67,7 @@
                       <svg class="h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
                       </svg>
-                      <span class="ml-2 w-0 flex-1 truncate">resume_back_end_developer.pdf</span>
+                      <span class="ml-2 w-0 flex-1 truncate">data.pdf</span>
                     </div>
                     <div class="ml-4 flex-shrink-0">
                       <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
@@ -79,7 +79,7 @@
                       <svg class="h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
                       </svg>
-                      <span class="ml-2 w-0 flex-1 truncate">coverletter_back_end_developer.pdf</span>
+                      <span class="ml-2 w-0 flex-1 truncate">data2.pdf</span>
                     </div>
                     <div class="ml-4 flex-shrink-0">
                       <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
@@ -87,6 +87,13 @@
                   </li>
                 </ul>
               </dd>
+            </div>
+            <div class="bg-gray-50 px-4 py-5 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Peta Lokasi</dt>
+              <div>
+                Peta
+              </div>
+              <!-- <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Billy</dd> -->
             </div>
           </dl>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="text-center lg:text-center">
         <h2 class="text-lg font-semibold text-indigo-600">Selamat Datang</h2>
         <p class="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Suku Dinas Perumahan Rakyat dan Kawasan Pemukiman</p>
-        <!-- <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">DKI Jakarta</p> -->
+        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Kota Administrasi Jakarta Timur</p>
       </div>
 
       <div class="mt-28 grid gap-10 grid-cols-1 sm:grid sm:grid-cols-5">
