a5776352cf35dd37a7f98291e32086080d07bdb5
Zahirw <wakidzahir@gmail.com>
2022-10-04 20:12:54 +0700

    add marker maps

--- src/components/Maps/Maps.vue
+++ src/components/Maps/Maps.vue
@@ -1,16 +1,46 @@
 <template>
-  <div class="h-screen relative">
+  <div class="h-96">
     <div id="map" class="h-full z-[1]"></div>
   </div>
 </template>
 
 <script setup>
   import { computed, ref, onMounted, onUnmounted, watch, reactive } from 'vue'
+  import marker from '../../assets/mapMarker.svg'
   import leaflet from 'leaflet'
 
+  let map
+  onMounted(() =>{
+    map = leaflet.map('map').setView([0, 0], 13)
+    leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    getGeoLocation()
+  })
+  
   const coords = ref(null)
   const fetchCoords = ref(null)
   const geoMarker = ref(null)
+
+  const initGeoLocation = (lat, lng) => {
+    map.setView([lat, lng], 13);
+    leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+  }
+
+  const plotGeoLocation = (lat, lng) => {
+    const customMarker = leaflet.icon({
+      iconUrl: marker,
+      iconSize: [35,35],
+    })
+    geoMarker.value = leaflet.marker([lat, lng], { icon: customMarker }).addTo(map)
+    map.setView([lat, lng], 10)
+  }
 
   const getGeoLocation = () => {
     fetchCoords.value = true
@@ -19,30 +49,22 @@
 
   const setCoords = (pos) => {
     fetchCoords.value = null
-    coords.lat = pos.coords.lat
-    coords.lng =  pos.coords.lng
-
+    initGeoLocation(pos.coords.latitude, pos.coords.longitude)
     const setSessionCoords = {
       lat: pos.coords.latitude,
       lng: pos.coords.longitude,
     }
     sessionStorage.setItem('coords', JSON.stringify(setSessionCoords))
 
-    const map = leaflet.map('map').setView([pos.coords.latitude, pos.coords.longitude], 13);
-    leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    coords.value = setSessionCoords
+
+    plotGeoLocation(pos.coords.latitude, pos.coords.longitude)
+
   } 
 
   const getLocError = (err) => {
     console.log(err)
   }
-
-  onMounted(() =>{
-    getGeoLocation()
-  })
 </script>
 
 <style>
